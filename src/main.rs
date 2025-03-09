3d1dded563024cead32be1f694032d784ca84dca
Luca <lagunasluca@protonmail.com>
2025-03-09 16:36:35 +0100

    Updating Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![no_std]
 #![no_main]
+#![deny(unsafe_code)]
 
 use panic_rtt_target as _;
 use rtt_target::{rtt_init_print, rprintln};
+// use embedded_hal::{delay::DelayNs, digital::OutputPin};
+// use microbit::{board::Board, hal::timer::Timer};
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
